a438f7e1927f3907020361b67fe4164d2d6204fc
Edris89 <edriskeyam82@hotmail.com>
2019-04-19 21:25:57 +0200

    Build 0.1

--- app/Http/Controllers/SnackyStatsController.php
+++ app/Http/Controllers/SnackyStatsController.php
@@ -12,48 +12,14 @@ class SnackyStatsController extends Controller
 {
     
     public function index(Request $request){
-   
-
         return View('index');
     }
 
 
-    public function getAllData(Request $request){
-        
-        
-        //dd($request->all());
-
-        $fromDate = date('2019-04-16' . ' 00:00:00', time()); 
-        $toDate = date('2019-04-17' . ' 23:59:59', time()); 
-        $allData = DB::table('snacky_stats')
-                    ->whereBetween('datumpje', array($fromDate, $toDate))->orderBy('desc')->get();
-        
-        
-        return response()->json($allData);
-
-        //return response(view('index',array('allData'=>$allData)),200, ['Content-Type' => 'application/json']);
-    }
-
-    public function getTodayPage(){
-        $nowInAmsterdam =  Carbon::now('Europe/Amsterdam')->toDateString();
-        
-        $data = SnackyStats::whereDate('datumpje', '=' ,$nowInAmsterdam )->get();
-        //$data = SnackyStats::where('created_at', '>=', $nowInAmsterdam . ' 00:00:00')->where('created_at', '<=', $nowInAmsterdam . ' 23:59:59');
-        
-
-        return View('getTodayPage');
-        
-    }
-
-
     public function getDataToday(){
-
         $nowInAmsterdam =  Carbon::now('Europe/Amsterdam')->toDateString();
         $today = DB::table('snacky_stats')->whereDate('datumpje', '=' ,$nowInAmsterdam)->orderByRaw('datumpje ASC')->get();
-        //$today = SnackyStats::whereDate('datumpje', '=' ,$nowInAmsterdam )->get();
-
         return response()->json($today);
-        
     }
 
 
@@ -63,36 +29,19 @@ class SnackyStatsController extends Controller
         return response()->json($yesterdayData);
     }
 
-    public function getYesterdayPage(){
-
-        //$nowInAmsterdam =  Carbon::now('Europe/Amsterdam')->format('Y-m-d');
-        return view('getYesterdayPage');
-    }
-
-
-    public function LastsevenDaysPage(){
-        
-        return view('lastsevendays');
-
-    }
-
     public function LastsevenDaysData(){
         $lastsevendays =  Carbon::now('Europe/Amsterdam')->subDays(7)->toDateString();
         $nowInAmsterdam =  Carbon::now('Europe/Amsterdam')->toDateString();
-
         $lastsevendaysData = SnackyStats::whereBetween('datumpje', [$lastsevendays, $nowInAmsterdam])->orderByRaw('datumpje ASC')->get();
-        
         if($lastsevendaysData != Null){
             return response()->json($lastsevendaysData);
         }
     }
 
-
     public function last30days(){
         $last30daysData = Carbon::now('Europe/Amsterdam')->subDays(30)->toDateString();
         $nowInAmsterdam =  Carbon::now('Europe/Amsterdam')->toDateString();
         $last30days = SnackyStats::whereBetween('datumpje', [$last30daysData, $nowInAmsterdam])->orderByRaw('datumpje ASC')->get();
-        
         if($last30days != Null){
             return response()->json($last30days);
         }
@@ -100,7 +49,6 @@ class SnackyStatsController extends Controller
 
     public function thismonth(){
         $thismonth = SnackyStats::where('datumpje', '>=', Carbon::now('Europe/Amsterdam')->startOfMonth())->orderByRaw('datumpje ASC')->get();
-        
         return response()->json($thismonth);
     }
 
@@ -113,10 +61,6 @@ class SnackyStatsController extends Controller
         $data = DB::table('snacky_stats')->latest('datumpje')->first();
         $dataJson = json_decode(json_encode($data),true);
         $open_or_closed_bool = $dataJson['open_or_closed'];
-
         return response()->json($open_or_closed_bool);
-        
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,12 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-//Route::get('/', 'SnackyStatsController@index');
 
 Route::get('/', array('as'=>'index', 'uses'=>'SnackyStatsController@index'));
-Route::get('/getData', array('as'=>'getData', 'uses'=>'SnackyStatsController@getAllData'));
-
 Route::get('/getDataToday', array('as'=>'getDataToday', 'uses'=>'SnackyStatsController@getDataToday'));
-Route::get('/today', array('as'=>'today', 'uses'=>'SnackyStatsController@getTodayPage'));
-
 Route::get('/getDataYesterday', array('as'=>'getDataYesterday', 'uses'=>'SnackyStatsController@getDataYesterday'));
-Route::get('/yesterday', array('as'=>'yesterday', 'uses'=>'SnackyStatsController@getYesterdayPage'));
-
-
-Route::get('/lastsevendays', array('as'=>'lastsevendays', 'uses'=>'SnackyStatsController@LastsevenDaysPage'));
 Route::get('/lastsevendaysData', array('as'=>'lastsevendaysData', 'uses'=>'SnackyStatsController@LastsevenDaysData'));
-
 Route::get('/last30days', array('as'=>'last30days', 'uses'=>'SnackyStatsController@last30days'));
 Route::get('/thismonth', array('as'=>'thismonth', 'uses'=>'SnackyStatsController@thismonth'));
 Route::get('/lastmonth', array('as'=>'lastmonth', 'uses'=>'SnackyStatsController@lastmonth'));
-
-
-
 Route::get('/checkOpenOrClosed', array('as'=>'checkOpenOrClosed', 'uses'=>'SnackyStatsController@checkOpenOrClosed'));
-Route::post('/postGetData', array('as'=>'postGetData', 'uses'=>'SnackyStatsController@postGetData'));
